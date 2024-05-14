6f5627569d5ada106186f7e2185e585e53201bfd
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2024-05-14 16:54:33 +0530

    Update firebase-config.js with new Firebase configuration
    This diff updates the Firebase configuration in the file firebase-config.js. The previous configuration has been commented out and replaced with a new one. The Firebase services being

--- src/Config/firebase-config.js
+++ src/Config/firebase-config.js
@@ -5,23 +5,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCMWNwP8iOUuonz32a5R5k5zA8pBk-aS7k",
-//   authDomain: "foodbymood-f20f7.firebaseapp.com",
-//   projectId: "foodbymood-f20f7",
-//   storageBucket: "foodbymood-f20f7.appspot.com",
-//   messagingSenderId: "88617605777",
-//   appId: "1:88617605777:web:c508673ff0096b41647d30",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBjtA5zx3d6Y_oD8jsKu0-hws1X9e3kXuo",
-  authDomain: "foodbymood-67d62.firebaseapp.com",
-  projectId: "foodbymood-67d62",
-  storageBucket: "foodbymood-67d62.appspot.com",
-  messagingSenderId: "496836300177",
-  appId: "1:496836300177:web:ec4660bbb54c3686211e30",
+  apiKey: "AIzaSyCMWNwP8iOUuonz32a5R5k5zA8pBk-aS7k",
+  authDomain: "foodbymood-f20f7.firebaseapp.com",
+  projectId: "foodbymood-f20f7",
+  storageBucket: "foodbymood-f20f7.appspot.com",
+  messagingSenderId: "88617605777",
+  appId: "1:88617605777:web:c508673ff0096b41647d30",
 };
+
+// **Alternate Firebase Database config file
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBjtA5zx3d6Y_oD8jsKu0-hws1X9e3kXuo",
+//   authDomain: "foodbymood-67d62.firebaseapp.com",
+//   projectId: "foodbymood-67d62",
+//   storageBucket: "foodbymood-67d62.appspot.com",
+//   messagingSenderId: "496836300177",
+//   appId: "1:496836300177:web:ec4660bbb54c3686211e30",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
